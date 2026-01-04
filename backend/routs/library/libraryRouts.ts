ddba2c0b03bace15a87dfaa7875ee628487c25e4
donatas100dl <donatas100dl@gmail.com>
2026-01-04 12:26:14 +0200

    updated routing and schemas

--- backend/routs/library/libraryRouts.ts
+++ backend/routs/library/libraryRouts.ts
@@ -4,23 +4,17 @@ import upload from '../../middleware/uploads'; // Assuming uploads.ts exports de
 
 import {
   addBook,
-  removeDupes,
   getBookPage,
   getBookInfo,
-  bookAddChapters,
-  getChapter,
   getStreamKey,
   verifYStreamKey,
 } from './libraryController';
 
 const router: Router = express.Router();
 
-// router.post('/add/book', addBook);
-// router.delete("/delete", removeDupes)
+router.post('/add/book', addBook);
 router.get('/get/books', getBookPage);
 router.get('/get/book/:id', getBookInfo);
-router.put('/put/ch/:bookId', bookAddChapters);
-router.put('/get/:bookId/', getChapter); //get chapter info and set lastest read book + chapterId
 router.get("/get/audio/key", protect, getStreamKey);
 router.post("/verify", verifYStreamKey)
 

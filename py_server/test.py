afbeec7c8c5508f15be1fc3bace502b201abf734
donatas100dl <donatas100dl@gmail.com>
2026-01-14 14:36:16 +0200

    chore: untrack entire py_server folder and its legacy files (now ignored)

--- py_server/test.py
+++
@@ -1,5 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.cuda.is_available())  # Should be True
-print(torch.cuda.get_device_name(0))  # "NVIDIA GeForce GTX 1660"
-print(torch.cuda.device_count())  # 1

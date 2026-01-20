564e22bce8cb48c91329e883011186dc64bb0be2
donatas100dl <donatas100dl@gmail.com>
2026-01-20 17:42:52 +0200

    added fovorite books frontend

--- frontend/src/components/AudioBookCard.tsx
+++ frontend/src/components/AudioBookCard.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { Play, Pause } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Play, Pause, Star } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Book } from "../types";
+import { useLibrary } from "../uttils/LibraryContext";
+import { useAuth } from "../uttils/AuthContex";
+import { start } from "repl";
+
+interface AudioBookCardProps extends Book {
+  removeBook?: (id: string) => void;
+  favoriteBooks?: Book[]
+}
+
 export const AudioBookCard = ({
   title,
   author,
@@ -9,14 +18,50 @@ export const AudioBookCard = ({
   coverImg,
   numberOfChapters,
   id,
-  isComplete
-}: Book) => {
+  isComplete,
+  favoriteBooks,
+  removeBook
+}: AudioBookCardProps) => {
+  const [isHover, setIsHover] =  useState<boolean>(false)
+  const [userFavorite, setUserFavorite] = useState<boolean>(false)
+  const {toggleFavoriteBook} = useLibrary()!
+
+  useEffect(() => {
+    if (!favoriteBooks) {
+      return
+    }
+    const isFavorite = favoriteBooks.some( book => book.id === id)
+    setUserFavorite(isFavorite)
+  },[]) 
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setUserFavorite(!userFavorite)
+    setIsHover(!isHover)
+    toggleFavoriteBook(id)
+    if (removeBook) {
+      removeBook(id)
+    }
+  }
   return <Link className="relative group rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 bg-gray-800 border border-gray-700" to={`/play/${id}/1`}>
     <div className="aspect-[4/4] relative">
       <img src={coverImg} alt={`${title} cover`} className="w-full h-full object-cover" />
       <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-60 transition-all duration-300 flex items-center justify-center">
         <button className="transform scale-0 group-hover:scale-100 transition-transform duration-300 w-8 h-8 rounded-full bg-purple-600 hover:bg-purple-700 flex items-center justify-center">
-          {true ? <Pause className="w-4 h-4 text-white" /> : <Play className="w-4 h-4 text-white ml-0.5" />}
+        <Play className="w-4 h-4 text-white ml-0.5" />
+        </button>
+        <button className="absolute z-50 top-0 right-0 m-4"
+        onMouseEnter={() => setIsHover(!isHover)}
+        onMouseLeave={() => {
+          if (userFavorite) {
+            setIsHover(true)
+            return
+          }
+          setIsHover(false)
+        }}
+        onClick={handleClick}
+        >
+          <Star className="transition-all duration-200" color="white" fill={(isHover || userFavorite) ? "white" : "transparent"}></Star>
         </button>
       </div>
     </div>
--- frontend/src/pages/LibraryPage.tsx
+++ frontend/src/pages/LibraryPage.tsx
@@ -1,10 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { AudioBookCard } from "../components/AudioBookCard";
 import { useLibrary } from "../uttils/LibraryContext";
 import { Book } from "../types";
 export const LibraryPage = () => {
-  const { library } = useLibrary()!; // Assumed library context
-  const books: Book[] = library?.books || [];
+  const { handleGetFavoriteBooks } = useLibrary()!; // Assumed library context
+  const [books, setBooks] = useState<Book[]>([])
+  
+  useEffect(() => {
+    const firstCall = async () => {
+      const favoriteBooks = await handleGetFavoriteBooks()
+      if (favoriteBooks && favoriteBooks.length > 0) {
+        const allBooks = favoriteBooks.map(fav => fav.book)
+        console.log(allBooks)
+        setBooks(allBooks)
+      }
+    }
+    firstCall()
+  }, [])
+  const handleRemoveFavorite = (bookId: string) => {
+    if (!bookId) {
+      return
+    }
+    const filteredBooks = books.filter(book => book.id != bookId)
+    setBooks(filteredBooks)
+  }
+
   return <div className="p-4">
     <h1 className="text-2xl font-bold text-gray-100 mb-4">My Library</h1>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6">
@@ -23,6 +43,8 @@ export const LibraryPage = () => {
             bookURL={book.bookURL}
             isComplete={book.isComplete}
             chList={book.chList}
+            favoriteBooks={books}
+            removeBook={handleRemoveFavorite}
           />
         ))
       )}
